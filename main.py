bee890e5721763bde243bd41fb20f0751d6f345f
abrahamNtd <agmc22mx@gmail.com>
2023-05-21 19:39:51 -0600

    [@abrahamNtd]This is the first try

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+def main():
+    my_input = os.environ["INPUT_MYINPUT"]
+    my_output = f"Hello {my_input}"
+    print(f"::set-output name=myOutput::{my_output}")
+
+
+if __name__ == "__main__":
+    main()
