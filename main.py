74f3cbbd0c2eb4dc9d572a059e1f46976222e3d9
abrahamNtd <agmc22mx@gmail.com>
2023-05-21 22:11:35 -0600

    testing the version 11

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 def main():
     print("Runing phyton inside of Docker")
+    for name, value in os.environ.items():
+        print("{0}: {1}".format(name, value))
 
 
 if __name__ == "__main__":
