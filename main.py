185e6618c35f2638857698ae1759a29237c6f356
abrahamNtd <agmc22mx@gmail.com>
2023-05-21 22:18:37 -0600

    testing the version 12

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     print("Runing phyton inside of Docker")
     for name, value in os.environ.items():
         print("{0}: {1}".format(name, value))
+    print ('Argument List:', str(sys.argv))
 
 
 if __name__ == "__main__":
