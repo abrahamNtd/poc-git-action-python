28f1021c0cd1bc27ffc512543296015dd3211466
abrahamNtd <agmc22mx@gmail.com>
2023-05-21 23:37:59 -0600

    testing the version 16

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import os
 import sys
+import os
+import shutil
+import traceback
+
+def list_directory(directory):
+    print("+ Listing directory: " + directory)
+    for item in os.listdir(directory):
+        print(" - " + item)
 
 def main():
     print("Runing phyton inside of Docker")
     for name, value in os.environ.items():
         print("{0}: {1}".format(name, value))
     print ('Argument List:', str(sys.argv))
+    #list_directory("/github/workspace")
+    list_directory(".")
 
 
 if __name__ == "__main__":
